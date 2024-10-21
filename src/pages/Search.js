440cfa14bae9aa5b7960ca4527601dc89e12a5dd
Marianne Anderson <mganderson13@gmail.com>
2024-10-21 14:06:03 -0500

    wrote Search function logic

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import BookCard from '../components/Card';
+
 const Search = () => {
-    return (
-        <h1>Search Page</h1>
-    );
+    const [search, setSearch] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [books, setBooks] = useState([]);
+  
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`);
+          const data = await res.json();
+          setBooks(data.items);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+  
+      if (search !== '') { // Only fetch data when search query is not empty
+        fetchData();
+      }
+    }, [search]); // Run useEffect whenever search query changes
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      setSearch(inputValue);
+    }
+return (
+    <>
+    <h1>Looking for a specific book or author or topic?</h1>
+    <h2>Search by typing in any keyword.</h2>
+    <form onSubmit={handleSubmit}>
+        <label>Keyword: 
+            <input type="text" name="keyword" value = {inputValue}
+        onChange={(e) => setInputValue(e.target.value )}/>
+        </label>
+        <button type="submit">Search</button>
+    </form>
+
+        <div>
+          <h2>Search Results:</h2>
+          <ul>
+            {books.map(book => (
+              <BookCard key={book.id} book={book}/>
+            ))}
+          </ul>
+        </div>
+    </>
+);
 }
 
 export default Search;
+
