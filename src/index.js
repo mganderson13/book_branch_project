182c239f7956751fdfeb89b95da6933157e43188
Marianne Anderson <mganderson13@gmail.com>
2025-02-03 21:00:32 -0600

    added firebase auth logic to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGqsJ2JeemDBje7t1uHcnhG9aMIhG3I0s",
+  authDomain: "book-branch.firebaseapp.com",
+  projectId: "book-branch",
+  storageBucket: "book-branch.firebasestorage.app",
+  messagingSenderId: "654504112586",
+  appId: "1:654504112586:web:588509fef5461baecf5f33"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
