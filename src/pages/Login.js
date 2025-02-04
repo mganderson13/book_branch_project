dfd578f38488ea65776975a1931db11da8b961e1
Marianne Anderson <mganderson13@gmail.com>
2025-02-03 21:40:40 -0600

    login page logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,48 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const logIn = async () => {
+        try{
+        await signInWithEmailAndPassword(getAuth(), email, password);
+        navigate('/home');
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+
     return (
+        <>
         <h1>Login Page</h1>
+        {error && <p className="error">{error}</p>}
+        <label for="username">Username</label>
+        <input 
+        type="text" 
+        id="username" 
+        name="username" 
+        placeholder='Your email address'
+        onChange={e => setEmail(e.target.value)}
+        />
+        <label for="password">Password</label>
+        <input 
+        type="password"
+        id="password"
+        name="password"
+        placeholder='Your password'
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        />
+        <button onClick={logIn} >Log In</button>
+        <br></br>
+        <Link to="/register">Don't have an account? Click here to register.</Link>
+        </>
     );
 }
 
